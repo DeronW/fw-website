bf1597fad59373a4c844c2951e359ee447a41f03
sunjiankun <sunjiankun@ucfgroup.com>
2016-11-04 17:59:53 +0800

    1104 17.59

---
+++ apps/eten/user-coupon/react/index.jsx
@@ -0,0 +1,30 @@
+'use strict';
+const API_PATH = document.getElementById('api-path').value;
+const Coupon = React.createClass({
+   render: function () {
+       return(
+           <div className="couponWrap">
+               <div className="couponContent">
+                   <div className="couponTitle">
+                       了解更多优惠券>
+                   </div>
+                   <div className="couponContainer">
+                        <div className="containerTop">
+                            <div className="containerTopLeft containerTopCom"></div>
+                            <div className="containerTopCenter containerTopCom"></div>
+                            <div className="containerTopRight containerTopCom"></div>
+                        </div>
+                       <div className="containerCenter">
+
+                       </div>
+                   </div>
+               </div>
+           </div>
+       )
+   }
+});
+
+
+
+       ReactDOM.render(<HeaderStatusBar />, document.getElementById('header-status-bar'));
+       ReactDOM.render(<Coupon />, document.getElementById('cnt'));
