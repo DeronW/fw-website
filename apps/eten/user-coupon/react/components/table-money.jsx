99f32b0c8499532430239e5ed7a3d18644ba0746
sunjiankun <sunjiankun@ucfgroup.com>
2016-11-11 14:00:01 +0800

    1359

--- apps/eten/user-coupon/react/components/table-money.jsx
+++ apps/eten/user-coupon/react/components/table-money.jsx
@@ -2,15 +2,11 @@ const TableMoney = React.createClass({
     getInitialState: function () {
         return ({
             isShow: false,
-            isListShow: false,
             transNum: 2,
             code: 4,
             status: 1,
+            page:1,
             couponProduce: {
-                result: [],
-                pagination: {}
-            },
-            friendsList: {
                 result: [],
                 pagination: {}
             }
@@ -20,12 +16,6 @@ const TableMoney = React.createClass({
         this.setState({
             isShow: !this.state.isShow
         })
-    },
-    handleListShow: function () {
-        this.setState({
-            isListShow: !this.state.isListShow
-        });
-        this.ajaxFriendsList();
     },
     handleTransformSuccess: function () {
         this.setState({
@@ -48,31 +38,16 @@ const TableMoney = React.createClass({
             data: {
                 page: 1,
                 limit: 8,
-                status: 2,
+                status: -1,
                 couponType: 1
             },
             type: 'get',
             success: function (data) {
                 if (data.code == 10000) {
                     this.setState({
-                        couponProduce: data.data.pageData
+                        couponProduce: data.data
                     })
                 }
-            }.bind(this)
-        })
-    },
-    ajaxFriendsList: function () {
-        $.ajax({
-            url: API_PATH + 'api/parttimeFinancialer/v1/searchFriends.json',
-            data: {
-                page: 1,
-                rows: 10
-            },
-            type: 'get',
-            success: function (data) {
-                _this.setState({
-                    friendsList: data.data.pageData
-                })
             }.bind(this)
         })
     },
@@ -82,85 +57,82 @@ const TableMoney = React.createClass({
         var showStyle = {
             display: _this.state.isShow ? "block" : "none"
         };
-        var listShowStyle = {
-            display: _this.state.isListShow ? "block" : "none"
+
+        var popLoginName = function (loginName, gcm, finalRole) {
+            var loginNameValue = '';
+            if (loginName != null && gcm.substring(0, 1) == 'A') {
+                if (finalRole == 4) {
+                    if (loginName.length <= 4) {
+                        loginNameValue = loginName.substring(0, 1) + "**"
+                    } else {
+                        loginNameValue =  loginName.substring(0, 2) + "**" + loginName.substring(loginName.length - 2, loginName.length);
+                    }
+                } else {
+                    loginNameValue = loginName;
+                }
+            } else if (loginName != null) {
+                if (loginName.length <= 4) {
+                    loginNameValue = loginName.substring(0, 1) + "**";
+                } else {
+                    loginNameValue = loginName.substring(0, 2) + "**" + loginName.substring(loginName.length - 2, loginName.length);
+                }
+            } else {
+                loginNameValue = '--';
+            }
+            return loginNameValue
         };
-        var popLoginName = function (sex, loginName, gcm, finalRole) {
+        var popRealName = function (realName,gcm,finalRole) {
             var sexValue = "";
             if (sex == 1) {
                 sexValue = "先生";
             } else {
                 sexValue = "女士";
             }
-            if (item.loginName != null && gcm.substring(0, 1) == 'A') {
-                if (finalRole == 4) {
-                    if (loginName.length <= 4) {
-                        return loginName.substring(0, 1) + "**"
-                    } else {
-                        return loginName.substring(0, 2) + "**" + loginName.substring(loginName.length - 2, loginName.length);
-                    }
-                } else {
-                    return loginName;
+            var realNameValue = '';
+            if(realName != null && gcm.substring(0,1) == 'A'){
+                if(finalRole==4){
+                    realNameValue = realName.substring(0,1)+sexValue;
+                }else{
+                    realNameValue = realName
                 }
-            } else if (loginName != null) {
-                if (loginName.length <= 4) {
-                    return loginName.substring(0, 1) + "**";
-                } else {
-                    return loginName.substring(0, 2) + "**" + loginName.substring(loginName.length - 2, loginName.length);
-                }
-            } else {
-                return;
+            } else if(realName != null) {
+                realNameValue = realName.substring(0,1)+sexhtml
+            }else{
+                realNameValue = "--";
             }
+            return realNameValue
+        };
+        var popMobile = function (mobile,gcm,finalRole) {
+            var mobileValue = '';
+            if (gcm.substring(0,1) == 'A') {
+                    if(finalRole==4){
+                        if(mobile!=null){
+                            mobileValue = mobile.substring(0,3)+"****"+item.mobile.substring(7,11);
+                        }else{
+                            mobileValue = "--"
+                        }
+                    }else{
+                        mobileValue = mobile
+                    }
+            } else {
+                if(mobile!=null){
+                    mobileValue = mobile.substring(0,3)+"****"+item.mobile.substring(7,11)
+                }else{
+                    mobileValue = "--";
+                }
+            }
+            return mobileValue
+        };
+        var popCreateTime = function (createTime) {
+            var time = '';
+            if(createTime != null){
+                time = createTime
+            }else{
+                time = "--"
+            }
+            return time
         };
 
-
-        var popList = function () {
-            var friend = function (item, index) {
-                return <li key={index}>
-                    <div className="centerCon1 centerCon">
-                        <input type="radio" name="single"/>
-                    </div>
-                    <div className="centerCon2 centerCon">{item.loginName}</div>
-                    <div className="centerCon3 centerCon">{item.realName}</div>
-                    <div className="centerCon4 centerCon">{item.mobile}</div>
-                    <div className="centerCon5 centerCon">{item.createTime}</div>
-                </li>
-            };
-            return <div className="listPopBg" style={listShowStyle}>
-                <div className="listPop">
-                    <div className="listTop">
-                        <div className="topLeft">请选择好友</div>
-                        <div className="topClose" onClick={_this.handleListShow}>关闭</div>
-                    </div>
-                    <div className="listCenter">
-                        <div className="centerTitle">
-                            <div className="centerCon1 centerCon">选择</div>
-                            <div className="centerCon2 centerCon">好友登录名</div>
-                            <div className="centerCon3 centerCon">好友姓名</div>
-                            <div className="centerCon4 centerCon">好友手机号</div>
-                            <div className="centerCon5 centerCon">好友注册时间</div>
-                        </div>
-                        <ul>
-                            {
-                                _this.state.friendsList.result.map(friend)
-                            }
-                        </ul>
-                        <div className="centerPage">
-                            <div className="pageCurrent">
-                                第<span>{_this.state.friendsList.pagination.pageNo}</span>页,共
-                                <p>{_this.state.friendsList.pagination.totalPage}</p>页
-                            </div>
-                            <div className="pageHome">首页</div>
-                            <div className="pageEnd">尾页</div>
-                        </div>
-                    </div>
-                    <div className="listFooter">
-                        <div className="sendBtn footerBtn">赠送</div>
-                        <div className="cancelBtn footerBtn" onClick={_this.handleListShow}>取消</div>
-                    </div>
-                </div>
-            </div>
-        };
 
         var totalOnePop = function () {
             return <div className="totalPop" style={showStyle}>
@@ -241,6 +213,6 @@ const TableMoney = React.createClass({
             panel = <TableMoneyPresent />
         }
 
-        return panel;
+        return panel
     }
 });
