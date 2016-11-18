fe5dea05c229e2e4214cb84a40f0409444978206
fengxiaoran <fengxiaoran@ucfgroup.com>
2016-11-18 15:25:45 +0800

    changed page style and position

--- apps/web/topic-xiao-fei-dai/javascripts/index.js
+++ apps/web/topic-xiao-fei-dai/javascripts/index.js
@@ -14,11 +14,6 @@ $(function () {
         'position': 'relative',
         "top": "0"
     });
-
-    // $(window).scroll(function () {
-    //     var scrollT=$(document).scrollTop()||document.documentElement.scrollTop || document.body.scrollTop;
-    //     console.log(scrollT);
-    // });
     $(window).scroll(function () {
         if ($(window).scrollTop() >420) {
             $("#nav").css({
@@ -48,7 +43,6 @@ $(function () {
             document.documentElement.scrollTop = document.body.scrollTop = value;
         }
     }
-
     var onesize1, onesize2, onesize3, onesize4, onesize5;
     getHeight();
     function getHeight() {
@@ -58,59 +52,30 @@ $(function () {
         onesize4 = $(".cooperationBox").height();
         onesize5 = $(".questionBox").height();
     }
-
     var iTop = $(".top").outerHeight();
-
     var iH = $(".centerNav").height();
-
     $(".centerNav ul li.list1").click(function () {
-        //starMove(document, {'scrollbar': iH + iTop-100});
         $(document).scrollTop(iH + iTop-100);
         $(this).children("a").addClass("on").parent().siblings().children("a").removeClass("on");
     });
     $(".centerNav ul li.list2").click(function () {
-        //starMove(document, {'scrollbar': onesize1 + iH + iTop-70});
         $(document).scrollTop(onesize1 + iH + iTop-70);
         $(this).children("a").addClass("on").parent().siblings().children("a").removeClass("on");
     });
     $(".centerNav ul li.list3").click(function () {
-        //starMove(document, {'scrollbar': onesize1 + onesize2 + iH + iTop - 30});
         $(document).scrollTop(onesize1 + onesize2 + iH + iTop - 30);
         $(this).children("a").addClass("on").parent().siblings().children("a").removeClass("on");
 
     });
     $(".centerNav ul li.list4").click(function () {
         $(document).scrollTop(onesize1 + onesize2 + onesize3 + iH + iTop - 30);
-        //starMove(document, {'scrollbar': onesize1 + onesize2 + onesize3 + iH + iTop - 30});
         $(this).children("a").addClass("on").parent().siblings().children("a").removeClass("on");
 
     });
     $(".centerNav ul li.list5").click(function () {
         $(document).scrollTop(onesize1 + onesize2 + onesize3 + onesize4 + iH + iTop - 30);
-        //starMove(document, {'scrollbar': onesize1 + onesize2 + onesize3 + onesize4 + iH + iTop - 30});
         $(this).children("a").addClass("on").parent().siblings().children("a").removeClass("on");
     });
-    function starMove(obj, oTarget) {
-        clearInterval(obj.timer);
-        obj.timer = setInterval(function () {
-            var iSpeed = 0;
-            var iCur = 0;
-            var bStop = true;
-            for (var attr in oTarget) {
-                iCur = css(obj, attr);
-                iSpeed = (oTarget[attr] - iCur) / 5;
-                iSpeed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
-                if (iCur != oTarget[attr]) {
-                    bStop = false;
-                    css(obj, attr, iCur + iSpeed);
-                }
-                flag = true;
-            }
-            if (bStop) {
-                clearInterval(obj.timer);
-            }
-        }, 0)
-    };
     $(window).scroll(function () {
         var st = $(window).scrollTop();
         if (st > (iTop + iH - 120) && st < (iH + iTop + onesize1 - 120)) {
