6dae01cbd0ca9e8cf974afce96161794c3c74ff0
delong <w.del@qq.com>
2016-12-16 11:50:56 +0800

    no message

--- gulpfile.web.js
+++ gulpfile.web.js
@@ -51,12 +51,10 @@ const WEB_APP_NAMES = [
     'user-coupon', //用户优惠券
     'user-messages', // 用户消息页面
     'user-bean', // 用户工豆
-    'user-score',//用户工分
+    'user-score', //用户工分
     'user-login-history', //用户登录日志页面
     // 其他页面
     'statistics', // 实时交易统计
-
-    // 'popong'
 ];
 
 
