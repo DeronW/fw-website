317d3a80440f5c68382c24717a0edd6a85f6633f
sunjiankun <sunjiankun@ucfgroup.com>
2016-12-28 15:14:57 +0800

    no message

--- apps/activity/2017-01-05/javascripts/index.js
+++ apps/activity/2017-01-05/javascripts/index.js
@@ -93,10 +93,10 @@ $(function () {
         $UserReady(function (is_login, user) {
             if (is_login) {
                 if (totalYearInvest == 0 || pullNewCount < 100 || myFriendYearInvest < 500000) {
-                    chartsText = `<div>1.6-3.30，您有效邀友 <em>${pullNewCount}</em> 人，好友累计年化投资<em>${myFriendYearInvest}</em>排名<em>${rankNum}</em>，当前无奖金可分，要努力哦！`;
+                    chartsText = "<div>1.6-3.30，您有效邀友 <em>"+pullNewCount+"</em> 人，好友累计年化投资<em>"+myFriendYearInvest+"</em>排名<em>"+rankNum+"</em>，当前无奖金可分，要努力哦！";
                 } else {
                     price = fixedPriceFun(totalYearInvest,myFriendYearInvest);
-                    chartsText = `<div>1.6-3.30，您有效邀友 <em>${pullNewCount}</em> 人，好友累计年化投资<em>${myFriendYearInvest}</em>排名<em>${rankNum}</em>，当前可分得<em>${price}</em>元奖金！`;
+                    chartsText = "<div>1.6-3.30，您有效邀友 <em>"+pullNewCount+"</em> 人，好友累计年化投资<em>"+myFriendYearInvest+"</em>排名<em>"+rankNum+"</em>，当前可分得<em>"+price+"</em>元奖金！";
                 }
                 $('.pcQuarterPack .quarterExplain,.mobileQuarterPack .quarterExplain').html(chartsText);
             } else {
