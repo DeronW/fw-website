12ee00c0a221302382911d75a7e52ee4bef5b35c
delong <w.del@qq.com>
2016-12-29 16:12:38 +0800

    fix uglify-js does not support IE8

--- tasks/javascripts.js
+++ tasks/javascripts.js
@@ -7,7 +7,9 @@ const plugins = require('gulp-load-plugins')();
 module.exports = javascripts = function (src_path, build_path, name, debug) {
     return gulp.src(src_path)
         .pipe(changed(build_path))
-        .pipe(debug ? plugins.util.noop() : js_uglify())
+        .pipe(debug ?
+            plugins.util.noop() :
+            js_uglify({output: {quote_keys: true, screw_ie8: false}}))
         .pipe(name ? concat(name, {newLine: ';\n'}) : plugins.util.noop())
         .pipe(gulp.dest(build_path));
 };
