35734be0a7939c18b2e6160884a1befbc6f83f9e
sunjiankun <sunjiankun@ucfgroup.com>
2017-01-04 20:42:11 +0800

    touchend

--- apps/activity/2017-01-05/javascripts/index.js
+++ apps/activity/2017-01-05/javascripts/index.js
@@ -56,12 +56,12 @@ $(function () {
         $FW.gotoSpecialPage("登录", loginUrl);
     });
     //移动端有效邀友判断
-    $(".mobilePerson").on("click", function (e) {
+    $(".mobilePerson").on("touchend", function (e) {
         $(".mobilePersonText").removeClass('hidden');
         e.preventDefault();
         return false
     });
-    $('body').on('click', function () {
+    $('body').on('touchend', function () {
         $(".mobilePersonText").addClass('hidden');
     });
 
