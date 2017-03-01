f18536c2bf16be9e6a0c27afd4ccbcfaeba2fafc
sunjiankun <sunjiankun@ucfgroup.com>
2017-03-01 12:03:11 +0800

    no message

--- lib/components/activity/board/poke-balloon-mobile.jsx
+++ lib/components/activity/board/poke-balloon-mobile.jsx
@@ -5,6 +5,11 @@ const BalloonBoom = React.createClass({
             giftPath: ''
         }
     },
+    componentWillReceiveProps(nextProps){
+        if(nextProps.number){
+            
+        }
+    },
     balloonBoomHandler(){
         var arr = ['images/balloonBoom.png', 'images/giftMobile.png'];
         var i = 0;
@@ -28,32 +33,84 @@ const BalloonBoom = React.createClass({
             }
         }, 40);
     },
-   render(){
-       let blueStyle = {
-           position: 'absolute',
-           left: '0',
-           top: '110px'
-       };
-       return <div className="ballBoom">
-           <img className="giftBoom" src={this.state.giftPath} alt=""/>
-           <img className="blueBalloon" onClick={this.balloonBoomHandler}
-                style={this.state.bluePath == 'images/giftMobile.png' ? blueStyle:{}} src={this.state.bluePath}/>
-       </div>
-   }
+    render(){
+        let blueStyle = {
+            position: 'absolute',
+            left: '0',
+            top: '110px'
+        };
+        return <div className="ballBoom">
+            <img className="giftBoom" src={this.state.giftPath} alt=""/>
+            <img className="blueBalloon" onClick={this.balloonBoomHandler}
+                 style={this.state.bluePath == 'images/giftMobile.png' ? blueStyle:{}} src={this.state.bluePath}/>
+        </div>
+    }
 });
 
 const PokeBalloonMobile = React.createClass({
+    getInitialState(){
+        return {
+            number: 1,
+            btnTab: '使用1次机会',
+            notOnceClick: 1,
+            notTenClick: 10
+        }
+    },
+    componentDidMount(){
+        $.get("./javascripts/once.json", (data)=> {
+            if (data.number < 10) {
+                this.setState({notTenClick: 0})
+            }
+            if(data.number < 1){
+                this.setState({notOnceClick: 0})
+            }
+        }, 'json')
+    },
+    changeNumberHandler(tab){
+        this.setState({btnTab: tab});
+        if(this.state.notClick){
+            if (tab == '使用1次机会') {
+                this.setState({number: 1})
+            } else if (tab == '使用10次机会') {
+                this.setState({number: 10})
+            }
+        }else{
+            this.setState({number:0})
+        }
+
+    },
     render(){
+        let notClick = {
+            width: '276px',
+            height: '102px',
+            background: 'url("images/notBtn.png") no-repeat',
+            lineHeight: '102px',
+            textAlign: 'center',
+            fontSize: '30px',
+            color: '#676767',
+            marginLeft: '25px'
+        };
+        let btn = (item, index)=> {
+            return <div key={index} className={this.state.btnTab == item?"active":""}
+                        style={this.state.notTenClick ? {} : notClick}
+                        onClick={()=>this.changeNumberHandler(item)}>{item}</div>
+        };
         return <div className="pokeBalloonMobile">
             <div className="ball">
-                <BalloonBoom path='images/blue.png'/>
+                <BalloonBoom path='images/blue.png' number={this.state.number}/>
             </div>
             <div className="ball2">
-                <BalloonBoom path='images/purple.png'/>
+                <BalloonBoom path='images/purple.png' number={this.state.number}/>
             </div>
             <div className="ball3">
-                <BalloonBoom path='images/pink.png'/>
+                <BalloonBoom path='images/pink.png' number={this.state.number}/>
             </div>
+            <div className="chanceBtn">
+                {
+                    ['使用1次机会', '使用10次机会'].map(btn)
+                }
+            </div>
+
         </div>
     }
 });
