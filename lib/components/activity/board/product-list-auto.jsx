6d7062e944acf84f7d8d22e9e4bb2b43a967d9ae
sunjiankun <sunjiankun@ucfgroup.com>
2017-03-06 18:16:37 +0800

    专题移动端

--- lib/components/activity/board/product-list-auto.jsx
+++ lib/components/activity/board/product-list-auto.jsx
@@ -1,7 +1,62 @@
 const ProductListAuto = React.createClass({
-   render(){
-       return <div className="productListAuto">
+    getInitialState(){
+        return {
+            products: []
+        }
+    },
+    componentDidMount(){
+        this.resortHandler();
+    },
+    resortHandler(){
+        $.get("./javascripts/getPersonDate.json", (data)=> {
+            this.setState({products: data.data.list})
+        }, "json")
+    },
+    render(){
+        let {products} = this.state, sum = products.length;
+        let group = (arr, size) => {
+            var r = [];
+            arr = arr || [];
+            for (var i = 0; i < arr.length; i += size) {
+                r.push(arr.slice(i, i + size))
+            }
+            return r
+        };
+        let column = (rows) => {
+            let fnCell = (cell, index) => {
+                return <div className="cell" key={index}>
+                    <img src={cell.picture} alt=""/>
 
-       </div>
-   }
+                    <div className="productName">{cell.goodsname}</div>
+                </div>
+            };
+            let fnRow = (row, index)=> {
+                return <div className={"row row-col-" + row.length} key={index}>{row.map(fnCell)}</div>
+            };
+            return <div>{rows.map(fnRow)}</div>
+        };
+        let items_c_1, items_c_2, items_c_3;
+        if (sum === 1 || (sum > 3 && sum % 3 !== 1)) {
+            if (sum % 3 == 1) items_c_1 = products.slice(Math.max(0, sum - 1));
+            if (sum % 3 == 0) items_c_1 = products;
+        }
+        if (sum === 2 || (sum > 3 && sum % 3 !== 0)) {
+            if (sum % 3 == 2) items_c_2 = products.slice(Math.max(0, sum - 2));
+            if (sum % 3 == 1) items_c_2 = products.slice(Math.max(0,sum - 4));
+        }
+        if (sum >= 3) {
+            let cur;
+            if (sum % 3 == 0) cur = sum;
+            if (sum % 3 == 1) cur = sum - 4;
+            if (sum % 3 == 2) cur = sum - 2;
+            items_c_3 = products.slice(0, Math.max(0, cur));
+        }
+        return <div className="productListAuto">
+            <div className="products-container">
+                {column(group(items_c_3, 3))}
+                {column(group(items_c_2, 2))}
+                {column(group(items_c_1, 1))}
+            </div>
+        </div>
+    }
 });
