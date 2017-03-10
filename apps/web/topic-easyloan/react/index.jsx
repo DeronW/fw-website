03cce9ce8356e0b41bce561200848ab36675b833
fengxiaoran <fengxiaoran@ucfgroup.com>
2017-03-10 18:37:53 +0800

    no message

--- apps/web/topic-easyloan/react/index.jsx
+++ apps/web/topic-easyloan/react/index.jsx
@@ -1,3 +1,3 @@
 $(function () {
-    ReactDOM.render(<HeaderStatusBar />, document.getElementById('header-status-bar'));
+    // ReactDOM.render(<HeaderStatusBar />, document.getElementById('header-status-bar'));
 });
