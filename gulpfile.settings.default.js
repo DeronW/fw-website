77eba75e75ead68f61c21ac86a02c7898cd4c076
sunjiankun <sunjiankun@ucfgroup.com>
2017-03-28 14:33:15 +0800

    develop

--- gulpfile.settings.default.js
+++ gulpfile.settings.default.js
@@ -1,6 +1,6 @@
 module.exports = {
     web: {
-        dev_api_path: '//fe.9888.cn/api/test-a/web/'
+        dev_api_path: '//fe.9888.cn/api/test-b/web/'
     },
     mall: {
         dev_api_path: '//fe.9888.cn/api/test-a/mall/'
