9558c33f53bce251c7b8b9c048f0548c9e447467
fengxiaoran <fengxiaoran@ucfgroup.com>
2017-04-13 17:20:28 +0800

    user-score page

--- apps/web/user-score/react/index.jsx
+++ apps/web/user-score/react/index.jsx
@@ -39,7 +39,7 @@ const Content = React.createClass({
         return (
             <div className="topNav">
                 <div className="scoreTips">
-                    注：此处为我在工场P2P和工场尊享共发出/收到的工分。
+                    注：此处为我在工场P2P和工场尊享共获得的工分。
                 </div>
                 <div className="topRow">
                     <div className="on">我的工分</div>
