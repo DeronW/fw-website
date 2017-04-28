57913899bafe8b4f0cdf7ca7617ec8cf13ca66d1
fengxiaoran <fengxiaoran@ucfgroup.com>
2017-04-28 14:39:56 +0800

    aboutus page

--- apps/p2p-web/aboutus/javascripts/index.js
+++ apps/p2p-web/aboutus/javascripts/index.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
+    var hash = location.hash;
+    function jumplink(ele) {
+        $(ele).addClass("active").siblings().removeClass("active")
+        $(".r-item").eq($(ele).index()).css({display: "block"}).siblings().css({display: "none"})
+        $(".aboutus").text($(ele).find(".l-content").text());
+        $(".a-left").height($(".a-right").height());
+    }
+    if(hash==="#aboutus"){
+        jumplink(hash);
+    }else if(hash==="#contactus"){
+        jumplink(hash);
+    }else if(hash==="#school"){
+        jumplink(hash);
+    }
     $(".left-item").each(function (index, item) {
         $(this).click(function () {
             $(this).addClass("active").siblings().removeClass("active")
@@ -18,9 +32,16 @@ $(document).ready(function () {
         })
     })
     // 百度地图API功能
-    var map = new BMap.Map("item2-box");    // 创建Map实例
-    map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
-    map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
-    map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
-    map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+    var map = new BMap.Map("bdmap");
+    var point = new BMap.Point(116.4347124,39.957053);
+    map.centerAndZoom(point,12);
+    var myGeo = new BMap.Geocoder();
+    myGeo.getPoint("北京市朝阳区朝阳门外大街18号", function(point){
+        if (point) {
+            map.centerAndZoom(point, 16);
+            map.addOverlay(new BMap.Marker(point));
+        }else{
+            alert("您选择地址没有解析到结果!");
+        }
+    }, "北京市");
 })
