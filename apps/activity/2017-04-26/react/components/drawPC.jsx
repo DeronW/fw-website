48aeedc69c22e2931b0b7b7e49e35293f0d5fcf7
sunjiankun <sunjiankun@ucfgroup.com>
2017-04-28 10:13:59 +0800

    0426

--- apps/activity/2017-04-26/react/components/drawPC.jsx
+++ apps/activity/2017-04-26/react/components/drawPC.jsx
@@ -1,14 +1,43 @@
 class drawPC extends React.Component{
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
+            isLogin:false,
+            monthNotice:"",
 
         }
+    }
+
+    componentDidMount(){
+        var _this = this;
+        $UserReady(function (isLogin, user) {
+
+        })
+    }
+    render(){
+        return (
+                <div>
+                   <PersonMonthLadder />
+                </div>
+            )
+
+    }
+}
+
+class PersonMonthLadder extends React.Component{
+    constructor(props){
+        super(props)
     }
     componentDidMount(){
 
     }
     render(){
+        return (
+                <div className="personMonthLadder">
+                    asdafa
+                </div>
+            )
+
 
     }
 }
