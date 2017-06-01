f3be90b3079051158b0e3b29eb060d53c5f03960
delong <w.del@qq.com>
2017-06-01 10:47:07 +0800

    refactory

--- gulpfile.js
+++ gulpfile.js
@@ -16,10 +16,10 @@ try {
     settings = {};
 }
 
-const SETTINGS = Object.assign({}, require('./gulpfile.settings.default.js'), settings);
+const SETTINGS = Object.assign({}, require('./gulpfiles/settings.default.js'), settings);
 // gulpfile 本地扩展配置
-require('./gulpfile.activity.js')(gulp, gt, SETTINGS);
-require('./gulpfile.web.js')(gulp, gt, SETTINGS);
-require('./gulpfile.loan-web.js')(gulp, gt, SETTINGS);
-require('./gulpfile.zx-web.js')(gulp, gt, SETTINGS);
-require('./gulpfile.p2p-web.js')(gulp, gt, SETTINGS);
+require('./gulpfiles/activity.js')(gulp, gt, SETTINGS);
+require('./gulpfiles/web.js')(gulp, gt, SETTINGS);
+require('./gulpfiles/loan-web.js')(gulp, gt, SETTINGS);
+require('./gulpfiles/zx-web.js')(gulp, gt, SETTINGS);
+require('./gulpfiles/p2p-web.js')(gulp, gt, SETTINGS);
