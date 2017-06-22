781df34f036856d22a3e3164a4f95c4fe0587a33
fengxiaoran <fengxiaoran@ucfgroup.com>
2017-06-22 14:51:19 +0800

    update

--- apps/activity/2017-07-13/react/components/julymobile.jsx
+++ apps/activity/2017-07-13/react/components/julymobile.jsx
@@ -32,7 +32,118 @@ class JulyMobile extends React.Component {
             ReactDOM.render(<PopStartOrEnd text="活动已结束"/>, document.getElementById("pop"))
         }
         return <div className="july-mobile-box">
-            this is mobile box
+            <div className="m-banner"></div>
+            <div className="m-coupon">
+                <div className="m-c-title">
+                    <img src="images/m-coupon-title.png"/>
+                </div>
+                <div className="m-c-tips">
+                    每周、限时抢高达千元返现、1%返息
+                </div>
+                <div className="m-c-text">
+                    <img src="images/m-get-coupon.png"/>
+                </div>
+                <div className="m-c-des">
+                    <div className="des-text">
+                        点击“去抢券”<br/>
+                        进入领券中心
+                    </div>
+                </div>
+            </div>
+            <div className="m-invite">
+                <div className="m-i-title">
+                    <img src="images/m-invite-title.png"/>
+                </div>
+                <div className="m-i-tips">
+                    注册7天内，累投年化额≥1000元算一个有效邀请
+                </div>
+                <div className="m-i-one">
+                    <div className="one-text">
+                        活动期间，成功邀请有效好友至少送50元。
+                        <a href="" className="one-detail">查看详情</a>
+                    </div>
+                </div>
+                <div className="m-i-two">
+                    <div className="two-text">
+                        根据活动内有效邀请数，每人可获对应最高档位奖励
+                    </div>
+                    <div className="two-num">
+                        <div className="num-left">
+                            有效邀请 <span className="num-yellow">1-19人</span>，奖励
+                        </div>
+                        <div className="num-right">
+                            有效邀请 <span className="num-red">≥20人</span>，奖励
+                        </div>
+                    </div>
+                </div>
+                <div className="m-i-three">
+                    <div className="three-title">
+                        活动内，成为人脉王的工友（有效邀请≥20）<br/>
+                        且新增好友累投年化额（不含自身）≥100万的工友<br/>
+                        可进入TOP5瓜分奖金！
+                    </div>
+                </div>
+                <div className="m-i-method">
+                    奖金分配方式：本人有效好友累投年化额/前5名有效好友累投<br/>
+                    年化总额，仅计算满足获奖资格的用户。
+                </div>
+            </div>
+            <div className="m-fight">
+                <div className="m-f-title">
+                    <img src="images/m-fight-title.png"/>
+                </div>
+                <div className="m-f-top">
+                    <div className="top-item">
+                        <div className="top-item-left">
+                            <img src="images/m-top1.jpg"/>
+                        </div>
+                        <div className="top-item-right top-r-one">
+
+                        </div>
+                    </div>
+                    <div className="top-item">
+                        <div className="top-item-left">
+                            <img src="images/m-top2.jpg"/>
+                        </div>
+                        <div className="top-item-right top-r-two">
+
+                        </div>
+                    </div>
+                    <div className="top-item">
+                        <div className="top-item-left">
+                            <img src="images/m-top3.jpg"/>
+                        </div>
+                        <div className="top-item-right top-r-three">
+
+                        </div>
+                    </div>
+                    <div className="top-item">
+                        <div className="top-item-left">
+                            <img src="images/m-top4.jpg"/>
+                        </div>
+                        <div className="top-item-right top-r-four">
+
+                        </div>
+                    </div>
+                    <div className="top-item">
+                        <div className="top-item-left">
+                            <img src="images/m-top5.png"/>
+                        </div>
+                        <div className="top-item-right top-r-five">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="m-bonus">
+                <div className="m-b-title">
+                    <img src="images/m-bonus-title.png"/>
+                </div>
+                <div className="m-n-tips">
+                    活动期间，团队累投年化额≥350万且排名前10的用户<br/>
+                    送出88万奖金！
+                </div>
+            </div>
         </div>
     }
 }
