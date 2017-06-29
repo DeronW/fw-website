2921810d948a92edd82bcba94440597db1df50d6
delong <w.del@qq.com>
2017-06-29 12:22:59 +0800

    update

--- gulpfiles/web.js
+++ gulpfiles/web.js
@@ -5,8 +5,8 @@ const PROJ = 'web';
 let APP_NAMES = [
     'guide', // 新手引导
     'app-download', // app 下载页面
-    'welcome-register', // 推广用户注册落地页面
-    'welcome-register-success', // 推广用户注册 成功页面
+    // 'welcome-register', // 推广用户注册落地页面
+    // 'welcome-register-success', // 推广用户注册 成功页面
 ];
 
 // 公告类页面
