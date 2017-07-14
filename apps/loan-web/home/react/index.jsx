8522673923e591d31f0ea63871c13e237bb23be7
刘桃桃 <liutaotao@ucfgroup.com>
2017-07-14 10:17:10 +0800

    fix bug

--- apps/loan-web/home/react/index.jsx
+++ apps/loan-web/home/react/index.jsx
@@ -37,7 +37,7 @@ class Content extends React.Component{
                             <div className="item-pic">
                                 <img src="../images/featureone.jpg" />
                             </div>
-                            <div class="item-text">
+                            <div className="item-text">
                                 <div className="t-f">简单</div>
                                 <div className="t-s">只需三步，轻松借钱</div>
                             </div>
@@ -121,7 +121,7 @@ class Content extends React.Component{
                                 <b className="gap-line"></b>
                                 <span className="info-item"><a href="/static/loan-web/aboutus/index.html#partner" target="_blank">合作伙伴</a></span>
                             </div>
-                            <p>友情链接：金融工场，易宝支付，百融金服，Face++</p>
+                            <p>友情链接：<a className="link-item" href="http://www.9888keji.com">金融工场</a>，<a className="link-item" href="http://www.yeepay.com">易宝支付</a>，<a className="link-item" href="http://www.100credit.com">百融金服</a>，<a className="link-item" href="https://www.faceplusplus.com.cn">Face++</a></p>
                         </div>
                         <div className="f-t">
                             <p className="copyright">Copyright©2017 深圳市众利财富管理有限公司 粤ICP备17034889号-1
--- apps/loan-web/lib/components/footer.jsx
+++ apps/loan-web/lib/components/footer.jsx
@@ -13,7 +13,7 @@ class Footer extends React.Component{
                                 <b className="gap-line"></b>
                                 <span className="info-item"><a href="/static/loan-web/aboutus/index.html#partner" target="_blank">合作伙伴</a></span>
                             </div>
-                            <p>友情链接：金融工场，易宝支付，百融金服，Face++</p>
+                            <p>友情链接：<a className="link-item" href="http://www.9888keji.com">金融工场</a>，<a className="link-item" href="http://www.yeepay.com">易宝支付</a>，<a className="link-item" href="http://www.100credit.com">百融金服</a>，<a className="link-item" href="https://www.faceplusplus.com.cn">Face++</a></p>
                         </div>
                         <div className="f-t">
                             <p className="copyright">Copyright©2017 深圳市众利财富管理有限公司 粤ICP备17034889号-1
@@ -26,3 +26,4 @@ class Footer extends React.Component{
                 </div>
     }
 }
+
