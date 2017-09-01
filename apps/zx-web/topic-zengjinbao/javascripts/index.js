6f62a4e3c2d2411e9889e695e811f5c7ad3d653e
liyanzhu <liyanzhu@ucfgroup.com>
2017-09-01 17:57:05 +0800

    Update

--- apps/zx-web/topic-zengjinbao/javascripts/index.js
+++ apps/zx-web/topic-zengjinbao/javascripts/index.js
@@ -1,3 +1,38 @@
 $(function () {
 
+  var FRAGMENTPOS = [447, 1560, 1996, 3119, 3406];
+
+  function toggleTab(selectedTab) {
+    $('.tab-group').children('.tab-item').removeClass('tab-item--active');
+    selectedTab.addClass('tab-item--active');
+  }
+
+  function scrollToFragment(tabNo) {
+    $(window).scrollTop(FRAGMENTPOS[tabNo]);
+  }
+
+  $(window).scroll(function scrollHandler () {
+    var scrollTop = $(window).scrollTop();
+    var scrollFragmentNo = 0;
+    for (var i = 0; i < FRAGMENTPOS.length; i++) {
+      if (scrollTop + 2 >= FRAGMENTPOS[i]) {
+        scrollFragmentNo = i;
+      } else {
+        break
+      }
+    }
+    toggleTab($('.tab-group').children().eq(scrollFragmentNo));
+  });
+
+  $('.tab-item').click(function() {
+    var selectedTab = $(this);
+    var selectedTabNo = selectedTab.children('i')[0].className.slice(-1);
+    scrollToFragment(selectedTabNo);
+    toggleTab(selectedTab);
+  })
+
+  $('.cover-text').click(function() {
+    $(window).scrollTop(0);
+  })
+
 });
