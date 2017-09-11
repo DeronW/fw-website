efcb1228f6c6c771d036156738474a318b00faf5
delong <w.del@qq.com>
2017-09-11 17:24:22 +0800

    add gold  page link

--- apps/keji-web/lib/components/user-center-sidebar.jsx
+++ apps/keji-web/lib/components/user-center-sidebar.jsx
@@ -96,9 +96,13 @@ class UserCenterSidebar extends React.Component {
                 <i className="ucp-icon icon-p2p"></i>
                 <span>我的微金</span>
             </a>
-            <a className="ucp-link" href="http://www.gongchangzx.com/account/home.shtml">
+            <a className="ucp-link" href="https://www.gongchangzx.com/account/home.shtml">
                 <i className="ucp-icon icon-zx"></i>
                 <span>我的尊享</span>
+            </a>
+            <a className="ucp-link" href="https://www.gongchangzx.com/gold/home.shtml">
+                <i className="ucp-icon icon-zx"></i>
+                <span>我的黄金</span>
             </a>
 
             <div className="ucp-horizon-line"></div>
