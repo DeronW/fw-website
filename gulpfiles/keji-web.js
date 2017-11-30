363f232599cd243d951192ad544d0822c5afc5ac
delong <w.del@qq.com>
2017-11-30 15:49:54 +0800

    refactory keji-web , use handlebars as default html template engine

--- gulpfiles/keji-web.js
+++ gulpfiles/keji-web.js
@@ -2,57 +2,27 @@ const eslint = require('gulp-eslint');
 
 const PROJ = 'keji-web';
 
-// 专题说明类页面
-const APP_NAMES = [{
-    name: 'guide', // 'guide', // 新手引导
-    html_engine: 'hbs'
-}, {
-    name: 'guide-cookbook', // 玩赚攻略页
-    html_engine: 'hbs'
-}, {
-    name: 'app-download', // app 下载页面
-    html_engine: 'hbs'
-}, {
-    name: 'update-browser', // IE8及一下版本浏览器的升级提示
-    html_engine: ''
-}]
+const APP_NAMES = [
+    'guide',// 新手引导
+    'guide-cookbook', // 玩赚攻略页
+    'app-download', // app 下载页面
+    'update-browser',// IE8及一下版本浏览器的升级提示
 
-// 公告类页面
-const NOTICE_PAGES = [{
-    name: 'notice-preservation', // 安全保障介绍页面
-    html_engine: 'hbs'
-}, {
-    name: 'notice-corporate-structure', // 信息披露页面
-    html_engine: 'hbs'
-}, {
-    name: 'notice-vip-prerogative', // 等级攻略页
-    html_engine: 'hbs'
-}, {
-    name: 'notice-user-protocol',//金融工场用户协议
-    html_engine: 'hbs'
-}, {
-    name: 'welcome', // 'welcome'//PC落地渠道页
-}]
+    'notice-preservation', // 安全保障介绍页面
+    'notice-corporate-structure', // 信息披露页面
+    'notice-vip-prerogative', // 等级攻略页
+    'notice-user-protocol',//金融工场用户协议
+    'welcome', // 'welcome'//PC落地渠道页
 
-// 专题说明类页面
-const TOPIC_PAGES = [{
-    name: 'topic-annual-commision', // A码 用户返利规则 佣金说明
-    html_engine: 'hbs'
-}, {
-    name: 'topic-hui-shang', // 徽商
-    html_engine: 'hbs'
-}, {
-    name: 'topic-gong-you-hui', //工友会专题页
-    html_engine: 'hbs'
-}, {
-    name: 'topic-hui-shang-guide', // 徽商 开户引导
-    html_engine: 'hbs'
-}]
+    'topic-annual-commision', // A码 用户返利规则 佣金说明
+    'topic-hui-shang', // 徽商
+    'topic-gong-you-hui', //工友会专题页
+    'topic-hui-shang-guide', // 徽商 开户引导
+]
 
 //用户界面
 const USER_PAGES = [{
     name: 'user-score', //用户工分
-    html_engine: 'hbs',
     project_components: [
         'header-status-bar.jsx',
         'alert.jsx',
@@ -69,16 +39,13 @@ const USER_PAGES = [{
     ]
 }]
 
-APP_NAMES.push(
-    ...NOTICE_PAGES,
-    ...TOPIC_PAGES,
-    ...USER_PAGES
-)
+APP_NAMES.push(...USER_PAGES)
 
 module.exports = function (gulp, generate_task, settings) {
     let common_config = {
+        html_engine: 'hbs',
         hbs_partials: [
-            'base', 'user-base', 'footer', 'header-nav-bar',
+            'base', 'user', 'footer', 'header-nav-bar',
             'growing-io', 'shortcut'
         ],
         project_components: [
@@ -114,21 +81,7 @@ module.exports = function (gulp, generate_task, settings) {
             cdn_prefix: `/static/${PROJ}/${i.name || i}/`,
         }, common_config))
 
-        gulp.task(`lint:${PROJ}:${i.name || i}`, gulp.series(() => {
-            return gulp.src([
-                `apps/${PROJ}/${i.name || i}/**/*.+(js|jsx)`,
-                '!node_modules/**',
-                '!**/jquery.*.js',
-                '!**.min.js'
-            ])
-                .pipe(eslint())
-                .pipe(eslint.result(result => null))
-                .pipe(eslint.format())
-                .pipe(eslint.failAfterError());
-        }))
     });
 
     gulp.task(`build:${PROJ}`, gulp.series(APP_NAMES.map(i => `${PROJ}:pack:${i.name || i}:revision`)))
-
-    gulp.task(`lint:${PROJ}`, gulp.series(APP_NAMES.map(i => `lint:${PROJ}:${i.name || i}`)))
 };
