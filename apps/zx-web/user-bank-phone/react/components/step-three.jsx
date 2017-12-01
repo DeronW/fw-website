1470705f33b16c0895a9e8107ac92d997513d8dc
sunjiankun <sunjiankun@ucfgroup.com>
2017-12-01 14:23:48 +0800

    fix

--- apps/zx-web/user-bank-phone/react/components/step-three.jsx
+++ apps/zx-web/user-bank-phone/react/components/step-three.jsx
@@ -1,21 +1,17 @@
-const StepThree = React.createClass({
-    getInitialState: function () {
-        return {}
-    },
-    render: function () {
+class StepThree extends React.Component {
+
+    render() {
         let successfulReturn = <a className="return"
-                                  href="http://www.gongchangzx.com/depository/recharge/toQRecharge.do">
+            href="/depository/recharge/toQRecharge.do">
             返回充值
         </a>
 
-        return (
-            <div className="thirdContent">
-                <div className="markedWords">
-                    <img src="images/success.png" alt=""/>
-                    恭喜您，手机号修改成功！
+        return <div className="thirdContent">
+            <div className="markedWords">
+                <img src="images/success.png" alt="" />
+                恭喜您，手机号修改成功！
                 </div>
-                {successfulReturn}
-            </div>
-        )
+            {successfulReturn}
+        </div>
     }
-})
+}
