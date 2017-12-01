5982b702dfc80141ad1709a0945100a3a978300f
liyanzhu <liyanzhu@ucfgroup.com>
2017-12-01 14:42:28 +0800

    Update: .hbs for aboutus

--- gulpfiles/settings.default.js
+++ gulpfiles/settings.default.js
@@ -9,7 +9,7 @@ module.exports = {
         dev_api_path: '//fe.gongzhangzx.com/api/test-c/zx-web'
     },
     "p2p-web": {
-        dev_api_path: '//fe.9888.cn/api/test-c/web'
+        dev_api_path: '//fe.gongchangp2p.com/api/test-c/p2p-web'
     },
     "keji-web": {
         dev_api_path: '//fe.9888keji.com/api/test-c/keji-web'
