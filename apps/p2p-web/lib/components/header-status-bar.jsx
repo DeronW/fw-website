8db5991960f0375024c5668c0ee92cb28fd7d270
delong <w.del@qq.com>
2017-12-28 11:22:58 +0800

    update

--- apps/p2p-web/lib/components/header-status-bar.jsx
+++ apps/p2p-web/lib/components/header-status-bar.jsx
@@ -88,7 +88,7 @@ class HeaderStatusBar extends React.Component {
 
         let p2pHeader = <div className="container">
             <div className="hsb-xin-dai">
-                <a href="http://www.9888keji.com" target="_blank">前往金融工场首页</a>
+                {/* <a href="http://www.9888keji.com" target="_blank">前往金融工场首页</a> */}
             </div>
             <div className="header-status-bar-app">
                 <a href="/static/p2p-web/app-download/index.html">APP 客户端</a>
